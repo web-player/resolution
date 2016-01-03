09102c2cbeffc5354374dc02fbece4fcb6c6cf2a
Amanskywalker <ak47su30ac@gmail.com>
2016-01-03 23:10:07 +0530

    modified:   classes/resolution.class.php 	modified:   classes/user.class.php

--- classes/resolution.class.php
+++ classes/resolution.class.php
@@ -6,31 +6,35 @@
 	*/
 	class res 
 	{
-		public:
-
-		$userid;	//userid of user
-		$title;		//resulatoin title
-		$text; 		//resolution text
-		$mode;		//resolution visibility mode
+		public $userid;		//userid of user
+		public $title;		//resulatoin title
+		public $text; 		//resolution text
+		public $mode;		//resolution visibility mode
 	
-		function read_table($uid)		//$uid == $userid
+		public function read_table($uid)		//$uid == $userid
 		{
+			include '/dbms/dbms_imp.php';
+			
 			$sql_query="SELECT * FROM `res` WHERE `uid`='$uid'";
 			$sql_result=$connection->query($sql_query);
 			$count=$sql_result->num_rows;
 
 			if ($count==0)		//no entry exist 
 			{
-				echo "F**k off";
+				echo "Nothing entered";
 			} 
 			else
 			{
 				while ($rows=$sql_result->fetch_array())
 				{
+					$this->userid=$uid;
+					$this->title=$rows[1];
+					$this->text=$rows[2];
+					$this->mode=$rows[3];
 					
+					echo "<p>$this->title</p></br><p>$this->text</p>";
 				}
-			}
-			
+			}	
 		}
 	}
 ?>
